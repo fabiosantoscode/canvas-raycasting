8c56830d9b255e34ad10c6ff407bf881c80dc20c
Fábio Santos <fabiosantosart@gmail.com>
2016-07-31 14:44:03 +0100

    room system

--- index.js
+++ index.js
@@ -13,12 +13,42 @@ var server = http.createServer((req, res) => {
     return statics(req, res)
 })
 
-var events = new events.EventEmitter
 var wss = new ws.Server({ server: server })
 
+var MAX_PER_ROOM = 20
+
+function room() {
+  var roomEvents = new events.EventEmitter
+  return {
+    count: 0,
+    add: function addToRoom(ws) {
+      this.count++
+      const onMove = (moveData) => { ws.send(moveData, () => {}) }
+      const onMessage = (message) => { roomEvents.emit('move', message) }
+      ws.on('message', onMessage)
+      roomEvents.on('move', onMove)
+      ws.on('error', (e) => { console.error(e) })
+      ws.on('close', () => {
+        this.count--
+        ws.removeListener('message', onMessage)
+        roomEvents.removeListener('move', onMove)
+      })
+    }
+  }
+}
+
+var rooms = (function push(arr, c) {
+  if (!--c) { return [] }
+  return arr.concat([room()])
+})([], 100)
+
 wss.on('connection', ws => {
-  ws.on('message', (message) => { events.emit('move', message) })
-  events.on('move', (moveData) => { ws.send(moveData, () => {}) })
+  for (var i = 0; i < rooms.length; i++) {
+    if (rooms[i].count < MAX_PER_ROOM) {
+      rooms[i].add(ws)
+      return
+    }
+  }
 })
 
 var port = +process.env['PORT'] || 8080
